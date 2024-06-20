57e3fe2be6c169512cd96bf7692e0d2723e35803
gpoell <gpoell.gp@gmail.com>
2024-06-20 11:53:48 +0100

    encapsulating server functionality in new State Machine component

--- src/containers/Desktop.py
+++ src/containers/Desktop.py
@@ -1,10 +1,7 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout
-from PyQt6.QtCore import QThreadPool
 from containers.ControlPanel.ControlPanel import ControlPanel
 from containers.Console.Console import Console
-from components.L9110HMotor import L9110HMotor
-from components.TactileSensor import TactileSensor
-from components.ThreadWorker import ThreadWorker
+from components.StateMachine import StateMachine
 
 class Desktop(QWidget):
     def __init__(self):
@@ -18,18 +15,14 @@ class Desktop(QWidget):
 
         # Create Components
         control_panel = ControlPanel()
-        self.threadpool = QThreadPool()
+        self.stateMachine = StateMachine()
         self.console = Console()
-        self.motor = L9110HMotor("127.0.0.1", 5000) # read this from config file
-        self.tactile_sensor = TactileSensor("127.0.0.1", 5000)
 
         # Connect Signals and Slots
-        self.tactile_sensor.signal.connect(self.console.tactile_data_format)
-        self.tactile_sensor.console_msg.connect(self.console.update)
-        control_panel.sensor_ctrls.connect_btn.clicked.connect(self.connect_sensor)
-        control_panel.sensor_ctrls.collect_btn.clicked.connect(self.collect_data)
-        control_panel.motor_ctrls.open_btn.clicked.connect(lambda: self.move_motor("open"))
-        control_panel.motor_ctrls.close_btn.clicked.connect(lambda: self.move_motor("close"))
+        self.stateMachine.tactile_sensor.sig_tactile_data.connect(self.console.tactile_data_format)
+        self.stateMachine.tactile_sensor.sig_console_msg.connect(self.console.update)
+        control_panel.motor_ctrls.sig_state_command.connect(self.stateMachine.exec)
+        control_panel.sensor_ctrls.sig_state_command.connect(self.stateMachine.exec)
 
         # Add Containers to Layouts
         top.addWidget(self.console)
@@ -43,16 +36,3 @@ class Desktop(QWidget):
         self.setLayout(main)
         self.setGeometry(175, 50, 600, 650)
         self.setWindowTitle('QMUL RBH')
-
-    def connect_sensor(self):
-        worker = ThreadWorker(self.tactile_sensor.connect)
-        self.threadpool.start(worker)
-
-    def collect_data(self):
-        worker = ThreadWorker(self.tactile_sensor.collect)
-        self.threadpool.start(worker)
-
-    def move_motor(self, direction):
-        if self.motor.state == 'inactive':
-            worker = ThreadWorker(self.motor.move, direction)
-            self.threadpool.start(worker)
