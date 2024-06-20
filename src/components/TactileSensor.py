6758c11f5f54be75c3fb736ccc5f1244f98bf2cf
gpoell <gpoell.gp@gmail.com>
2024-06-20 11:52:22 +0100

    improving sensor and motor functionality to be used by State Machine

--- src/components/TactileSensor.py
+++ src/components/TactileSensor.py
@@ -1,49 +1,95 @@
+"""
+The TactileSensor collects data from the Hall Effect sensor on the gripper.
+
+Attributes:
+
+    state <str>: defines the state of the sensor
+    collect_data <float>: captures the data collected by the sensor
+    collect_flag <bool>: flag indicating when to collect data based on sample
+
+Methods:
+    connect: tests the connection with the Esp32 server
+    collect: collects a sample of data from the Hall Effect sensor
+    read: reads data from Hall Effect sensor and emits to Console
+"""
+
 from PyQt6.QtCore import QObject, pyqtSignal as Signal
 from components.EspClient import EspClient
+from utils.datalog import write_csv
 
 class TactileSensor(QObject):
 
-    signal = Signal(tuple, name='tactileData')
-    console_msg = Signal(str, name="consoleMessage")
+    sig_tactile_data = Signal(tuple, name='tactileData')
+    sig_console_msg = Signal(str, name="consoleMessage")
 
-    def __init__(self, host, port):
+    def __init__(self):
         super().__init__()
-        self.client = EspClient(host, port)
+        self.state = 'idle'
         self.collect_data = []
         self.collect_flag = False
 
     def connect(self):
-        self.client.connect()
-        self.client.send_data("collect")
-        batch = self.client.receive_data()
-		# Receive Data Batches
+        """Continuously reads tactile sensor data and emits them to the Console"""
+        
+        # Connect to server, send command, and update state
+        client = EspClient()
+        client.connect()
+        client.send_data("connect")
+        self.state = "connected"
+
+        # Read acknowledge bit response from server
+        batch = client.receive_data()
+
+		# Continuously process data until null bit terminator is received
         while batch != '':
-            batch = self.client.receive_data()
+            batch = client.receive_data()
             if not batch : break
+            print(batch) # need to solve for byte length
             batch = batch.split(',')
+
+            # Collect data when the collect button is pressed
             if self.collect_flag: self.collect_data.append([batch[0], batch[1], batch[2]])
-            self.signal.emit((batch[0], batch[1], batch[2]))
-        self.client.close()
+
+            # Emit tactile sensor data
+            self.sig_tactile_data.emit((batch[0], batch[1], batch[2]))
+
+        # Close client connection and reset state
+        client.close()
+        self.state = "idle"
+
 
     def collect(self, sample=20):
+        """Collects a sample (default=20) of tactile sensor data and stores it in CSV file"""
+
+        if self.state != "connected":
+            print("[WARNING]: Tactile Sensor must be reading data in order to collect data.")
+            return
+        
+        # Start collecting data
         self.collect_flag = True
-        self.console_msg.emit("------COLLECTED--------")
+        self.sig_console_msg.emit("------COLLECTED--------")
+
+        # Wait for collected data to reach sample length
         while len(self.collect_data) < sample: continue
+
+        # Stop collecting data
         self.collect_flag = False
-        self.console_msg.emit("------COLLECTED--------")
+        self.sig_console_msg.emit("------COLLECTED--------")
+
+        # Temporary print to test
         for i in self.collect_data:
             print(i)
         print(len(self.collect_data))
-        self.write_csv(self.collect_data)
+
+        # Write data to CSV file
+        write_csv(self.collect_data)
+
+        # Reset collected data list
         self.collect_data = []
 
-    def write_csv(self, data):
-
-        path = "data/tactile_sensor_data.csv"
-        object = 'cherry'
-
-        with open(path, 'a+') as f:
-            for value in data:
-                f.write(f"{value[0]},{value[1]},{value[2]},{object}")
-                f.write("\n")
-            f.close()
+    def disconnect(self):
+        """Sends command to stop reading data from sensor"""
+        client = EspClient()
+        client.connect()
+        client.send_data("disconnect")
+        client.close()
