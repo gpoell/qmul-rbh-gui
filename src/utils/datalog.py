014cfe5258a52bb7b4a262054eb13a3800906571
gpoell <gpoell.gp@gmail.com>
2024-07-17 12:14:01 +0100

    adding method for classifying object and refactoring write csv method

--- src/utils/datalog.py
+++ src/utils/datalog.py
@@ -1,16 +1,24 @@
-def data_format_sensor(data):
-    for index, vals in enumerate(data):
-        temp = vals.split(',')
-        data[index] = temp
-    
-    return data
+import pandas as pd
+import pickle
 
-def write_csv(data):
-    path = "data/tactile_sensor_data.csv"
-    object = 'apple'
+def write_csv(data, object="", file="tactile_sensor_data.csv", dir="data"):
+    """Writes data to a csv file at the specified path"""
 
+    path = data + '/' + file
     with open(path, 'a+') as f:
         for value in data:
             f.write(f"{value[0]},{value[1]},{value[2]},{object}")
             f.write("\n")
         f.close()
+
+def classify_object(data, file="model.pkl", dir="data"):
+    """Predicts the classification of an object based using the ML model"""
+    
+    path = data + '/' + file
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+
+    # Create Data Frame
+    df = pd.DataFrame(data=[data], columns=["tactile_x", "tactile_y", "tactile_z"])
+
+    return model.predict(df)
