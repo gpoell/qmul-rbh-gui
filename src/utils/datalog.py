ff2b1a7b291a7a4f0fcc03407776c6c0d6cee611
gpoell <gpoell.gp@gmail.com>
2024-07-31 10:47:19 +0100

    adding methods to compute vector magnitude and absolute values

--- src/utils/datalog.py
+++ src/utils/datalog.py
@@ -6,8 +6,9 @@ def write_csv(data, object, file="tactile_sensor_data.csv", dir="data"):
 
     path = dir + '/' + file
     with open(path, 'a+') as f:
-        for value in data:
-            f.write(f"{value[0]},{value[1]},{value[2]},{object}")
+        for row in data:
+            f.writelines(f"{value}," for value in row)
+            f.write(object)
             f.write("\n")
         f.close()
 
@@ -19,7 +20,7 @@ def classify_object(data, file="model.pkl", dir="data"):
     with open(path, 'rb') as f:
         model = pickle.load(f)
 
-    df = pd.DataFrame(data=[data], columns=["tactile_x", "tactile_y", "tactile_z"])
+    df = pd.DataFrame(data=[data], columns=["tactile_x", "tactile_y", "tactile_z", "abs_x", "abs_y", "abs_z", "magnitude"])
     prediction = model.predict(df)
     
     return decode_prediction(prediction)
@@ -29,5 +30,5 @@ def decode_prediction(prediction):
 
     prediction = int(prediction)
     # Temporary - future configuration file will determine result
-    if prediction == 0: return "Syringe"
+    if prediction == 0: return "Balloon"
     if prediction == 1: return "Tennis Ball"
