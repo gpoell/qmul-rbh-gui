7955e1871ac790de5aec44499bb6894b32b9905f
gpoell <gpoell.gp@gmail.com>
2024-07-31 16:34:57 +0100

    adding new client module containing the WiFiClient, TactileSensor, and L9110HMotor classes

--- src/utils/client.py
+++ src/utils/client.py
@@ -3,96 +3,215 @@ NAME
 	client.py
 
 DESCRIPTION
-	This module provides classes and functions that establish wireless socket connections to a server (i.e. ESP32)
+	This module provides classes and functions that establish wireless socket connections to a server (i.e. ESP32).
 
 CLASSES
 	WiFiClient
+	TactileSensor
+	L9110HMotor
 		
-FUNCTIONS
-
 """
 
 import socket
 import yaml
+from utils.datalog import processTactileData
+from PyQt6.QtCore import QObject, pyqtSignal as Signal
+
 
 class WiFiClient:
-	def __init__(self, func):
-		self._set_connection_config()
-		self.func = func
+	"""
+	Used as a decorator for functions to standardize how the client connects to the server, sends commands,
+	receives and processes server data, and closes the client connection once complete.
 
-	def __call__(self, *args, **kwargs):
-		self._connect()
-		status = self._send_data(kwargs['command'])
-		if status == 400:
-			self._close()
-			raise ValueError("Server received a bad request.")
-		if status == 404:
-			self._close()
-			raise ValueError("Server did not recognize command.")
-		if status != 200:
-			self._close()
-			raise ValueError("Server experienced an issue.")
-		while batch != '':
-			batch = self.socket.recv(kwargs['buffSize']).decode('UTF-8')
-			if not batch : break
-			return self.func(*args, **kwargs)
+	PARAMETERS
+		command <string>: command to be sent to the server
+		buffersize <int>: the size of the buffer to receive from the server (default = 64)
 
-		self._close()
+	METHODS
+			_setConnection
+			_connect
+			_sendCommand
+			_receiveData
+			_close
+	"""
+	def __init__(self, **kwargs):
+		self._setConnection()
+		self.command = kwargs['command']
+		self.buffersize = kwargs['buffersize']
+
+	def __call__(self, func):
+		"""
+		Returns a decorated function that performs the client communication and executes the function
+		wrapped by the decorated class while processing the server data.
+		"""
+		def wrapper(*args, **kwargs):
+			try:
+				self._connect()
+				batch = self._sendCommand(self.command)
+				self._receiveData(func, *args)
+			except Exception:
+				raise Exception
+			finally:
+				self._close()
+		return wrapper
 
 	def _connect(self):
-		"""Establishes connection with device using host and port details defined in settings.yaml"""
+		"""
+		Establishes connection with the server the using host and port details defined in settings.yaml
+		and sets the socket property to retain the communication thread.
+		"""
 		try:
 			self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.socket.connect((self.host, self.port))
 		except Exception:
 			raise Exception("Failed to connect to device. Check settings and ensure device is listening.")
 	
-	def _send_data(self, msg):
-		"""Send command to server and return the status response"""
+	def _sendCommand(self, msg):
+		"""Sends the message argument to server and returns an integer status response"""
 		try:
 			self.socket.sendall(msg.encode("UTF-8"))
 			self.socket.send("\0".encode("UTF-8"))
 			self.socket.shutdown(1)
 			status = self.socket.recv(64).decode('UTF-8')
-			return int(status)
-		except Exception as e:
-			self.socket.close()
-			raise e
+			status = int(status)
+			return status
+		except Exception:
+			self._close()
+			raise Exception("Failed to send command to the server")
 
-	def _receive_data(self, batch, buffSize):
-		"""Return batches of data until data is no longer available"""
+	def _receiveData(self, func, *args):
+		"""
+		Processes the function argument while reading batches of data from the server.
+		
+		Parameters
+			func: a function reference that processes the batches of data
+			*args: additional arguments required for the function (used to pass back self)
+
+		"""
+		batch = True
 		while batch != '':
-			batch = self.socket.recv(buffSize).decode('UTF-8')
+			batch = self.socket.recv(self.buffersize).decode('UTF-8')
 			if not batch : return
-			yield batch
+			func(*args, batch)
 	
 	def _close(self):
-		"""Close the connection"""
+		"""Closes the connection to the server."""
 		self.socket.shutdown(0)
 		self.socket.close()
 
-	def _set_connection_config(self):
-		"""Set the host and port details defined in the settings.yaml"""
+	def _setConnection(self):
+		"""Sets the host and port details defined in the settings.yaml"""
 		with open('src/settings.yaml', 'r') as file:
 			conf = yaml.safe_load(file)
 			self.host = conf['client']['host']
 			self.port = conf['client']['port']
 
-	def send_command(self, command, buffSize=64, processDataFunc=None):
-		"""Sends a command to the server and optionally processes the data using the provided callback"""
-		self._connect()
-		status = self._send_data(command)
-		if status == 400:
-			self._close()
-			raise ValueError("Server received a bad request.")
-		if status == 404:
-			self._close()
-			raise ValueError("Server did not recognize command.")
-		if status != 200:
-			self._close()
-			raise ValueError("Server experienced an issue.")
+
+class TactileSensor(QObject):
+	"""
+	The TactileSensor contains methods for reading, calibrating, and collecting data from the Hall Effect sensor on the gripper.
+
+	METHODS
+		read
+		collect
+		_collectTactileData
+		disconnect
+		calibrate
+	"""
+
+	sig_tactile_data = Signal(tuple, name='tactileData')
+	sig_console_msg = Signal(dict, name="consoleMessage")
+
+	def __init__(self):
+		super().__init__()
+		self.state = 'idle'
+		self.tactileData = []
+		self.collectFlag = False
+
+	@WiFiClient(command="connect", buffersize=64)
+	def read(self, batch):
+		"""
+		Reads data from the tactile sensor and emits it to the console.
+
+		PARAMETERS
+			batch: string of 4 tactile data values (X, Y, Z, buffer message padding)
+		"""
+
+		# Split the data, remove the buffer message padding, and format the values to 2 decimal precision
+		batch = batch.split(',')
+		if len(batch) < 4: return
+		del batch[-1]
+		batch = [f"{float(num):.2f}" for num in batch]
+
+		# Emit tactile sensor data
+		self.sig_tactile_data.emit((batch[0], batch[1], batch[2]))
+
+	@WiFiClient(command="collect", buffersize=64)
+	def _collectTactileData(self, batch):
+		"""Collects a sample of tactile data from the gripper."""
 		
-		if processDataFunc:
-			(processDataFunc(batch) for batch in self._receive_data(status, buffSize))
+		# Split the data, remove the buffer message padding, and format the values to 2 decimal precision
+		batch = batch.split(',')
+		if len(batch) < 4: return
+		del batch[-1]
+		batch = [f"{float(num):.2f}" for num in batch]
 		
-		self._close()
+		self.tactileData.append(batch)
+
+	def collect(self, settings):
+		"""
+		Processes the collected tactile data by writing it to a csv file
+		or using the data model to classify the object.
+		"""
+		# Collect the sample of tactile data
+		self._collectTactileData()
+		
+		# Process the collected data based on the GUI mode and classifier
+		# settings = self.settings['gripper']['tactile']
+		processTactileData(settings, self.tactileData)
+
+		# Reset tactile data list
+		self.tactileData = []
+
+	@WiFiClient(command="disconnect", buffersize=64)
+	def disconnect(self, batch):
+		"""Sends command to disconnect the active client reading data from the gripper."""
+		self.console_message["header"] = "info"
+		self.console_message["body"] = f"Response from server: {batch}."
+
+	@WiFiClient(command="calibrate", buffersize=64)
+	def calibrate(self, batch):
+		"""Sends command calibrate the tactile sensors on the gripper."""
+		self.console_message["header"] = "info"
+		self.console_message["body"] = f"Response from server: {batch}."
+
+
+class L9110HMotor(QObject):
+	"""
+	The L9110HMotor contains methods for opening and closing the gripper.
+
+	METHODS
+		open
+		close
+	"""
+	sig_console_msg = Signal(dict, name="consoleMessage")
+
+	def __init__(self):
+		super().__init__()
+		self.state = 'idle'
+
+	@WiFiClient(command="open", buffersize=64)
+	def open(self, batch):
+		"""
+		Sends command to open the gripper and logs responses to the console.
+		"""
+		self.console_message["header"] = "info"
+		self.console_message["body"] = f"Response from server: {batch}."
+
+	@WiFiClient(command="close", buffersize=64)
+	def close(self, batch):
+		"""
+		Sends command to close the gripper and logs responses to the console.
+		"""
+		self.console_message["header"] = "info"
+		self.console_message["body"] = f"Response from server: {batch}."
